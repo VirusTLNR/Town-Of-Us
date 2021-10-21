bb42a78fca85bdbc7d854195f8ce245f0a351e64
Kevin Binswanger <kbinswanger@indeed.com>
2021-10-20 22:03:07 -0500

    fix(Assassin): Fix Prophet's color

--- source/Patches/Roles/Assassin.cs
+++ source/Patches/Roles/Assassin.cs
@@ -24,7 +24,7 @@ namespace TownOfUs.Roles
             { "Spy", new Color(0.8f, 0.64f, 0.8f, 1f) },
             { "Snitch", new Color(0.83f, 0.69f, 0.22f, 1f) },
             { "Altruist", new Color(0.4f, 0f, 0f, 1f) },
-            { "Prophet", new Color(0.19f, 0.09f, 0.20f, 1f)}
+            { "Prophet", new Color(0.69f, 0.15f, 1f, 1f)}
         };
 
         public Dictionary<byte, string> Guesses = new Dictionary<byte, string>();
